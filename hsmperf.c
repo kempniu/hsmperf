9432aae9d5242e100ac9310b1589a05f8dfc935c
Michał Kępień <github@kempniu.pl>
2015-07-15 11:46:20 +0200

    Implement benchmarking

--- hsmperf.c
+++ hsmperf.c
@@ -39,9 +39,12 @@
 
 #include "pkcs11.h"
 #include <dlfcn.h>
+#include <errno.h>
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include <unistd.h>
 
 #define OUT_OF_MEMORY_IF(cond) \
@@ -64,9 +67,11 @@
 		} \
 	}
 
+#define TIMESPEC_TO_NANOSECS(ts) (ts.tv_sec * 1000000000LL + ts.tv_nsec)
+
 const char *usage_message =
 	"Usage: hsmperf -l /path/to/libpkcs11.so"
-	" [ -s slot ]\n";
+	" [ -s slot ] [ -c iterations ]\n";
 
 char *pkcs11_lib_path = NULL;
 void *pkcs11_lib_handle = NULL;
@@ -76,6 +81,9 @@ struct {
 	void *ptr;
 } pkcs11_symbols[] = {
 	{ "C_CloseSession", NULL },
+	{ "C_DigestFinal", NULL },
+	{ "C_DigestInit", NULL },
+	{ "C_DigestUpdate", NULL },
 	{ "C_Finalize", NULL },
 	{ "C_GetSlotList", NULL },
 	{ "C_Initialize", NULL },
@@ -86,6 +94,7 @@ struct {
 };
 
 CK_SLOT_ID slot = 0;
+unsigned int iterations = 1000;
 
 void
 resolve_pkcs11_symbols(void)
@@ -145,6 +154,69 @@ login(CK_SESSION_HANDLE session, CK_BYTE *pin)
 }
 
 void
+benchmark(CK_SESSION_HANDLE session, FILE *feed, CK_MECHANISM_TYPE type,
+	  unsigned int digest_len, const char *mechanism_name)
+{
+	CK_MECHANISM mechanism;
+	CK_ULONG digestlen = digest_len;
+	unsigned char input[256];
+	size_t input_read;
+	unsigned char *digest;
+	struct timespec start, end;
+	unsigned int *timings;
+	unsigned int tmin = UINT_MAX, tmax = 0;
+	unsigned long long ttotal = 0;
+	unsigned int i;
+	int pct, last_pct = -1;
+
+	mechanism.mechanism = type;
+	mechanism.pParameter = NULL;
+	mechanism.ulParameterLen = 0;
+
+	digest = malloc(digest_len);
+	OUT_OF_MEMORY_IF(!digest);
+	timings = malloc(sizeof(int) * iterations);
+	OUT_OF_MEMORY_IF(!timings);
+	for (i = 0; i < iterations; i++) {
+		pct = 100 * i / iterations;
+		if (pct > last_pct) {
+			printf("\rTesting %s... %d%%", mechanism_name, pct);
+			fflush(stdout);
+			last_pct = pct;
+		}
+		input_read = fread(input, sizeof(input), 1, feed);
+		if (input_read != 1) {
+			fprintf(stderr, "Error getting random bytes\n");
+			free(timings);
+			return;
+		}
+		clock_gettime(CLOCK_MONOTONIC, &start);
+		PKCS11_CALL(C_DigestInit, session, &mechanism);
+		PKCS11_CALL(C_DigestUpdate, session, input, sizeof(input));
+		PKCS11_CALL(C_DigestFinal, session, digest, &digestlen);
+		clock_gettime(CLOCK_MONOTONIC, &end);
+		timings[i] = TIMESPEC_TO_NANOSECS(end);
+		timings[i] -= TIMESPEC_TO_NANOSECS(start);
+	}
+
+	for (i = 0; i < iterations; i++) {
+		if (timings[i] < tmin)
+			tmin = timings[i];
+		if (timings[i] > tmax)
+			tmax = timings[i];
+		ttotal += timings[i];
+	}
+
+	printf("\r%16s: min %.6f msec, max %.6f msec, avg %.6f msec\n",
+	       mechanism_name,
+	       (float) tmin / 1000000, (float) tmax / 1000000,
+	       (float) ttotal / iterations / 1000000);
+
+	free(digest);
+	free(timings);
+}
+
+void
 logout(CK_SESSION_HANDLE session)
 {
 	PKCS11_CALL(C_Logout, session);
@@ -167,7 +239,7 @@ parse_options(int argc, char *argv[])
 {
 	int opt;
 
-	while ((opt = getopt(argc, argv, "l:s:")) != -1) {
+	while ((opt = getopt(argc, argv, "l:s:c:")) != -1) {
 		switch (opt) {
 		case 'l':
 			pkcs11_lib_path = malloc(strlen(optarg) + 1);
@@ -176,6 +248,9 @@ parse_options(int argc, char *argv[])
 			break;
 		case 's':
 			slot = (CK_SLOT_ID) atoi(optarg);
+			break;
+		case 'c':
+			iterations = atoi(optarg);
 			break;
 		default:
 			fprintf(stderr, "%s", usage_message);
@@ -194,6 +269,7 @@ main(int argc, char *argv[])
 {
 	CK_SESSION_HANDLE session;
 	char *pin = NULL;
+	FILE *urandom;
 
 	parse_options(argc, argv);
 
@@ -210,6 +286,12 @@ main(int argc, char *argv[])
 	pin = getpass("Enter PIN: ");
 	if (strlen(pin))
 		login(session, (CK_BYTE *) pin);
+	urandom = fopen("/dev/urandom", "r");
+	if (!urandom) {
+		perror("Failed to open /dev/urandom");
+		exit(EXIT_FAILURE);
+	}
+	fclose(urandom);
 	if (strlen(pin))
 		logout(session);
 	end_session(session);
